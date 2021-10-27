d7bfe234455472e50eddfe019c8282e8fc5f61d3
Ziv Serphos <zivfromisrael@gmail.com>
2021-10-27 13:24:59 +0300

    added db.json file

--- back/index.js
+++ back/index.js
@@ -1,4 +1,3 @@
-const {Person , Player , GoalKeeper} = require("./soccerClasses")
 const express = require("express")
 const app = express();
 const cors = require('cors')
@@ -9,7 +8,7 @@ const errorHandlers = require("./handlers/errorHandlers")
 
 app.use(cors({origin: "*"}))
 app.use(express.json())
-app.use("/player" , handleUserReq , playerRouter)
+app.use("/player", playerRouter)
 app.use(errorHandlers)
 
 
@@ -20,11 +19,5 @@ app.listen(port, function() {
 app.get('/', function(req, res) {
 res.send('hello world!');
 });
-
-
-
-const avi = new Person("ziv" , "serphos" , "100000" , "34 " , ":-)")
-const aviv = new GoalKeeper("aviv" , "yamin" , "1500000" , "25" , "Sabih" , true , "never")
-const nadav = new Player("nadav" , "cohen" ,"10000" , "25" , "38848808" , "right" , "Striker" , "CCCCCCCCCCCCCC")
 
   
--- back/routers/player.js
+++ back/routers/player.js
@@ -1,4 +1,56 @@
+const {Person , Player , GoalKeeper} = require("../soccerClasses")
 const express = require("express");
 const playerRouter = express.Router();
+const fs = require("fs")
+const path = require("path")
+
+
+playerRouter.put("/create" , async (req ,res) => {
+    console.log("im here")
+    const data = req.body
+    const player = new Player(data.firstName , data.sureName , data.salary , data.age , data.id , data.strongLeg , data.position , data.celebrationSentence)
+    // const fileData = fs.readFile("db.json" , 'utf8' , await (req ,res) => {
+    //     console.log("")
+    // })
+    fs.appendFileSync("db.json" , JSON.stringify(player))
+    res.send(player)
+})
+
+const avi = new Person("ziv" , "serphos" , "100000" , "34 " , ":-)")
+const aviv = new GoalKeeper("aviv" , "yamin" , "1500000" , "25" , "Sabih" , true , "never")
+const nadav = new Player("nadav" , "cohen" ,"10000" , "25" , "38848808" , "right" , "Striker" , "CCCCCCCCCCCCCC")
+
+//console.log(avi)
+
+//localhost:8080/player/create
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = playerRouter
